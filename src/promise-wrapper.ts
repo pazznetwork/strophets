788882715b7f4b3f6f229bae210d5f97d9bee6c2
CodeBast4rd <20125737+CodeBast4rd@users.noreply.github.com>
2022-11-14 12:19:44 +0100

    chore: types cleanup

--- src/promise-wrapper.ts
+++
@@ -1,7 +0,0 @@
-export interface PromiseWrapper<T> {
-  isResolved: boolean;
-  isPending: boolean;
-  isRejected: boolean;
-  resolve?: (value: T | PromiseLike<T>) => void;
-  reject: (reason?: any) => void;
-}
